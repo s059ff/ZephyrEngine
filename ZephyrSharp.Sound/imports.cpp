5f8c7701f204e6c9fd53ce32b5528a2e49dc5cea
s059ff <s059ff@gmail.com>
2024-02-28 13:17:09 +0900

    外部ライブラリの参照先ディレクトリパスを変更
    * Web からダウンロードしたそのままのディレクトリ構成で library/ 以下にライブラリを配置することを想定

--- ZephyrSharp.Sound/imports.cpp
+++ ZephyrSharp.Sound/imports.cpp
@@ -2,6 +2,6 @@
 #pragma comment(lib, "dxguid.lib")
 #pragma comment(lib, "winmm.lib")
 //
+#pragma comment(lib, "libogg.lib")
 #pragma comment(lib, "libvorbis_static.lib")
 #pragma comment(lib, "libvorbisfile_static.lib")
-#pragma comment(lib, "libogg_static.lib")
