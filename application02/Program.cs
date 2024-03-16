9727f0bc108f1b39f9a5e1af13b5e6b269376ad4
s059ff <s059ff@gmail.com>
2024-03-16 18:30:50 +0900

    application02 を修正
    * コマンドライン引数で 通常モード, トレーニングモード, モデル評価モードを指定できるようにした
      * 通常モード: `null`
      * トレーニングモード: `"training"`
      * モデル評価モード: `"evaluation"`
    * ResetScene() を別ファイルに移動

--- application02/Program.cs
+++ application02/Program.cs
@@ -1,14 +1,11 @@
 //#define DebugMode
-#define TrainingMode
 
+using System;
+using System.Runtime.CompilerServices;
 using ZephyrSharp.GameSystem;
-using ZephyrSharp.GameSystem.Components;
 using ZephyrSharp.Graphics;
 using ZephyrSharp.Input;
-using ZephyrSharp.Linalg;
 using ZephyrSharp.Sound;
-using ZephyrTools.CommandRunner;
-using ZephyrTools.GameSystemMonitor;
 using static EngineScript;
 using static GameScript;
 
@@ -16,15 +13,21 @@ class Program
 {
     public static bool ResetSceneSignal = false;
 
-    static void Main(string[] args)
+    static void Main()
     {
+        var args = new
+        {
+            Mode = Environment.GetCommandLineArgs().Length > 1 ? Environment.GetCommandLineArgs()[1].ToLower() : null,
+        };
+        assert(args.Mode == null || args.Mode == "training" || args.Mode == "evaluation");
+
         Window window = new Window();
         window.Create("application02", DisplayWidth, DisplayHeight);
-#if TrainingMode
-        window.EnableBackgroundRunning = true;
-#else
-        window.EnableBackgroundRunning = false;
-#endif
+
+        if (args.Mode == "training" || args.Mode == "evaluation")
+            window.EnableBackgroundRunning = true;
+        else
+            window.EnableBackgroundRunning = false;
 
         GraphicsDevice.Instance.Create(window, FullScreen);
         SoundDevice.Instance.Create(window.Handle);
@@ -34,38 +37,38 @@ class Program
         var monitor = GameSystemMonitor.Execute();
 #endif
 
-        System.Func<bool> sceneResetCondition = () =>
-        {
-            bool condition1 = Entity.Find(e => (e.Name != null) && (e.Name.StartsWith("player"))) == null;
-            bool condition2 = Entity.Find(e => (e.Name != null) && (e.Name.StartsWith("enemy"))) == null;
-            return condition1 || condition2;
-        };
-
         window.Updated += () =>
         {
-#if TrainingMode
-            if (ResetSceneSignal)
+            if (args.Mode == "training" || args.Mode == "evaluation")
             {
-                ResetScene();
-                ResetSceneSignal = false;
+                if (ResetSceneSignal)
+                {
+                    Scene.ResetScene(args.Mode);
+                    ResetSceneSignal = false;
+                }
             }
-#else
-            bool flag = false;
-            flag |= Entity.Find(e => (e.Name != null) && (e.Name.StartsWith("player"))) == null;
-            //flag |= Entity.Find(e => (e.Name != null) && (e.Name.StartsWith("enemy"))) == null;
-            if (flag)
+            else
             {
-                ResetScene();
+                bool flag = false;
+                flag |= Entity.Find(e => (e.Name != null) && (e.Name.StartsWith("player"))) == null;
+                //flag |= Entity.Find(e => (e.Name != null) && (e.Name.StartsWith("enemy"))) == null;
+                if (flag)
+                {
+                    Scene.ResetScene(args.Mode);
+                }
             }
-#endif
 
-#if TrainingMode
-            bool rendering = pressed(Keyboard.KeyCode.F11) | pressed(Keyboard.KeyCode.F12);
-            bool vsync = pressed(Keyboard.KeyCode.F11);
-#else
-            bool rendering = true;
-            bool vsync = true;
-#endif
+            bool rendering, vsync;
+            if (args.Mode == "training")
+            {
+                rendering = pressed(Keyboard.KeyCode.F11) | pressed(Keyboard.KeyCode.F12);
+                vsync = pressed(Keyboard.KeyCode.F11);
+            }
+            else
+            {
+                rendering = true;
+                vsync = true;
+            }
             EngineScript.update();
             GameScript.update();
             Entity.BroadcastMessage(UpdateMessage);
@@ -87,7 +90,7 @@ class Program
             EngineScript.initialize();
             GameScript.initialize();
 
-            ResetScene();
+            Scene.ResetScene(args.Mode);
 
             window.Start();
         }
@@ -106,142 +109,6 @@ class Program
             GraphicsDeviceContext.Instance.Dispose();
             GraphicsDevice.Instance.Dispose();
             SoundDevice.Instance.Dispose();
-        }
-    }
-
-    static void ResetScene()
-    {
-        Entity.Clear();
-
-        Entity system = Entity.Instantiate();
-        system.Name = "system";
-        system.Attach(new SystemComponent());
-
-        Entity debugger = Entity.Instantiate();
-        debugger.Name = "debugger";
-        debugger.Attach(new LoggerComponent());
-        debugger.Attach(new DebugInformationDisplayComponent());
-
-        Entity gamespace = Entity.Instantiate();
-        gamespace.Name = "gamespace";
-        gamespace.Attach(new GameSpaceComponent(20000));
-
-        Entity camera = Entity.Instantiate();
-        camera.Name = "camera";
-        camera.Attach(new TransformComponent());
-        camera.Attach(new TrackingCameraComponent());
-        camera.Attach(new FixedPointCameraComponent());
-        camera.Attach(new SoundObserverComponent() { EffectRange = 10000, SonicSpeed = SonicSpeed * 2 });
-        camera.Get<TrackingCameraComponent>().Activate();
-
-        Entity camera2d = Entity.Instantiate();
-        camera2d.Name = "camera2d";
-        camera2d.Attach(new TransformComponent());
-        camera2d.Attach(new CameraComponent());
-
-        Entity projector = Entity.Instantiate();
-        projector.Name = "projector";
-        projector.Attach(new ProjectorComponent());
-        projector.Get<ProjectorComponent>().SetPerspectiveMode(PI / 3, DisplayAspect, 1.0f, 4096.0f);
-
-        Entity projector2d = Entity.Instantiate();
-        projector2d.Name = "projector2d";
-        projector2d.Attach(new ProjectorComponent());
-        projector2d.Get<ProjectorComponent>().SetOrthogonalMode(DisplayAspect * 2.0f, 2.0f, 0, 1);
-
-        Entity light = Entity.Instantiate();
-        light.Name = "light";
-        light.Attach(new TransformComponent());
-        light.Attach(new LightComponent());
-        light.Get<LightComponent>().LookAt(new Vector3(-1, 1, -1), new Vector3(0, 0, 0));
-
-        Entity sky = Entity.Instantiate();
-        sky.Name = "sky";
-        sky.Attach(new TransformComponent());
-        sky.Attach(new SkyComponent());
-
-        Entity ground = Entity.Instantiate();
-        ground.Name = "ground";
-        ground.Attach(new TransformComponent());
-        ground.Attach(new CollisionComponent());
-        ground.Attach(new GroundComponent());
-
-#if false
-        Entity cloud = Entity.Instantiate();
-        cloud.Name = "cloud";
-        cloud.Attach(new CloudComponent(randoms(distmap("res/texture/cloud_distribution.png"), 10000).Select(tuple =>
-        {
-            float x = tuple.Item1 - 0.5f;
-            float z = tuple.Item2 - 0.5f;
-            var length = Entity.Find("gamespace").Get<GameSpaceComponent>().SpaceLength;
-            x *= length;
-            z *= length;
-            float y = normal(1000, 100);
-            return new Vector3(x, y, z);
-        }).ToArray()));
-#endif
-
-        {
-            Entity entity = Entity.Instantiate();
-            entity.Name = "player";
-            entity.Attach(new TransformComponent());
-            entity.Attach(new PhysicsComponent());
-            entity.Attach(new CollisionComponent());
-            entity.Attach(new AircraftComponent("FA-22_Raptor"));
-            entity.Attach(new JetComponent());
-            entity.Attach(new SoundComponent(JetSound));
-            entity.Attach(new AircraftAvionicsComponent(Friend));
-            entity.Attach(new SquadronComponent("Gargoyle"));
-#if TrainingMode
-            entity.Attach(new LearnablePilotComponent());
-#else
-            entity.Attach(new PlayerPilotComponent());
-#endif
-            entity.Attach(new EnvironmentObservationComponent());
-            entity.Attach(new WindComponent());
-            entity.Attach(new AircraftHUDComponent());
-
-            entity.Get<TransformComponent>().Position = new Vector3(0, 2000, -2000);
-            entity.Get<SoundComponent>().LoopPlay();
-        }
-
-        for (int i = 0; i < 0; i++)
-        {
-            Entity entity = Entity.Instantiate();
-            entity.Name = string.Format("friend{0}", i + 1);
-            entity.Attach(new TransformComponent());
-            entity.Attach(new PhysicsComponent());
-            entity.Attach(new CollisionComponent());
-            entity.Attach(new AircraftComponent("FA-22_Raptor"));
-            entity.Attach(new JetComponent());
-            entity.Attach(new SoundComponent(JetSound));
-            entity.Attach(new AircraftAvionicsComponent(Friend));
-            entity.Attach(new SquadronComponent("Gargoyle"));
-            entity.Attach(new NonPlayerPilotComponent());
-
-            entity.Get<TransformComponent>().Position = new Vector3(normal(0, 100), 2000, normal(-2000, 100));
-            entity.Get<SoundComponent>().LoopPlay();
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            Entity entity = Entity.Instantiate();
-            entity.Name = string.Format("enemy{0}", i + 1);
-            entity.Attach(new TransformComponent());
-            entity.Attach(new PhysicsComponent());
-            entity.Attach(new CollisionComponent());
-            entity.Attach(new AircraftComponent("Su-37_Terminator"));
-            entity.Attach(new JetComponent());
-            entity.Attach(new SoundComponent(JetSound));
-            entity.Attach(new AircraftAvionicsComponent(Enemy));
-            entity.Attach(new SquadronComponent("Cyclops"));
-            entity.Attach(new NonPlayerPilotComponent());
-
-            entity.Get<TransformComponent>().Matrix.RotateX(uniform(-PI, PI));
-            entity.Get<TransformComponent>().Matrix.RotateY(uniform(-PI, PI));
-            entity.Get<TransformComponent>().Matrix.RotateZ(uniform(-PI, PI));
-            entity.Get<TransformComponent>().Position = new Vector3(normal(0, 100), 2000, normal(1000, 100));
-            entity.Get<SoundComponent>().LoopPlay();
         }
     }
 }
---
+++ application02/Scene.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using ZephyrSharp.GameSystem;
+using ZephyrSharp.GameSystem.Components;
+using ZephyrSharp.Linalg;
+
+using static EngineScript;
+using static GameScript;
+
+class Scene
+{
+    const int FriendCount = 0;
+    const int EnemyCount = 0;
+
+    public static void ResetScene(string mode)
+    {
+        Entity.Clear();
+        GC.Collect();
+
+        Entity system = Entity.Instantiate();
+        system.Name = "system";
+        system.Attach(new SystemComponent());
+
+        Entity debugger = Entity.Instantiate();
+        debugger.Name = "debugger";
+        //debugger.Attach(new LoggerComponent());
+        debugger.Attach(new DebugInformationDisplayComponent());
+
+        Entity gamespace = Entity.Instantiate();
+        gamespace.Name = "gamespace";
+        gamespace.Attach(new GameSpaceComponent(20000));
+
+        Entity camera = Entity.Instantiate();
+        camera.Name = "camera";
+        camera.Attach(new TransformComponent());
+        camera.Attach(new TrackingCameraComponent());
+        camera.Attach(new FixedPointCameraComponent());
+        camera.Attach(new SoundObserverComponent() { EffectRange = 10000, SonicSpeed = SonicSpeed * 2 });
+        camera.Get<TrackingCameraComponent>().Activate();
+
+        Entity camera2d = Entity.Instantiate();
+        camera2d.Name = "camera2d";
+        camera2d.Attach(new TransformComponent());
+        camera2d.Attach(new CameraComponent());
+
+        Entity projector = Entity.Instantiate();
+        projector.Name = "projector";
+        projector.Attach(new ProjectorComponent());
+        projector.Get<ProjectorComponent>().SetPerspectiveMode(PI / 3, DisplayAspect, 1.0f, 4096.0f);
+
+        Entity projector2d = Entity.Instantiate();
+        projector2d.Name = "projector2d";
+        projector2d.Attach(new ProjectorComponent());
+        projector2d.Get<ProjectorComponent>().SetOrthogonalMode(DisplayAspect * 2.0f, 2.0f, 0, 1);
+
+        Entity light = Entity.Instantiate();
+        light.Name = "light";
+        light.Attach(new TransformComponent());
+        light.Attach(new LightComponent());
+        light.Get<LightComponent>().LookAt(new Vector3(-1, 1, -1), new Vector3(0, 0, 0));
+
+        Entity sky = Entity.Instantiate();
+        sky.Name = "sky";
+        sky.Attach(new TransformComponent());
+        sky.Attach(new SkyComponent());
+
+        Entity ground = Entity.Instantiate();
+        ground.Name = "ground";
+        ground.Attach(new TransformComponent());
+        ground.Attach(new CollisionComponent());
+        ground.Attach(new GroundComponent());
+
+        {
+            Entity entity = Entity.Instantiate();
+            entity.Name = "player";
+            entity.Attach(new TransformComponent());
+            entity.Attach(new PhysicsComponent());
+            entity.Attach(new CollisionComponent());
+            entity.Attach(new AircraftComponent("FA-22_Raptor"));
+            entity.Attach(new JetComponent());
+            entity.Attach(new SoundComponent(JetSound));
+            entity.Attach(new AircraftAvionicsComponent(Friend));
+            entity.Attach(new SquadronComponent("Gargoyle"));
+            entity.Attach(new EnvironmentObservationComponent());
+            if (mode == "training" || mode == "evaluation")
+                entity.Attach(new LearnablePilotComponent());
+            else
+                entity.Attach(new PlayerPilotComponent());
+            // entity.Attach(new WindComponent());
+            entity.Attach(new AircraftHUDComponent());
+
+            //Quaternion q = new Quaternion(new Vector3(1.0f, -1.0f, 1.0f).Normalize(), deg2rad(135));
+            Quaternion q = new Quaternion(new Vector3(1.0f, 0.0f, 0.0f), deg2rad(45));
+            entity.Get<TransformComponent>().Matrix._Matrix3x3 = new Matrix3x3(q);
+            entity.Get<TransformComponent>().Matrix.Position = new Vector3(0, 2000, 0);
+
+
+            entity.Get<SoundComponent>().LoopPlay();
+        }
+
+        for (int i = 0; i < FriendCount; i++)
+        {
+            Entity entity = Entity.Instantiate();
+            entity.Name = string.Format("friend{0}", i + 1);
+            entity.Attach(new TransformComponent());
+            entity.Attach(new PhysicsComponent());
+            entity.Attach(new CollisionComponent());
+            entity.Attach(new AircraftComponent("FA-22_Raptor"));
+            entity.Attach(new JetComponent());
+            entity.Attach(new SoundComponent(JetSound));
+            entity.Attach(new AircraftAvionicsComponent(Friend));
+            entity.Attach(new SquadronComponent("Gargoyle"));
+            entity.Attach(new NonPlayerPilotComponent());
+
+            entity.Get<TransformComponent>().Position = new Vector3(normal(0, 100), 2000, normal(-2000, 100));
+            entity.Get<SoundComponent>().LoopPlay();
+        }
+
+        //var x = linspace(-8000, +8000, EnemyCount);
+        var x = new float[] { 0 };
+        for (int i = 0; i < EnemyCount; i++)
+        {
+            Entity entity = Entity.Instantiate();
+            entity.Name = string.Format("enemy{0}", i + 1);
+            entity.Attach(new TransformComponent());
+            entity.Attach(new PhysicsComponent());
+            entity.Attach(new CollisionComponent());
+            entity.Attach(new AircraftComponent("Su-37_Terminator"));
+            entity.Attach(new JetComponent());
+            entity.Attach(new SoundComponent(JetSound));
+            entity.Attach(new AircraftAvionicsComponent(Enemy));
+            entity.Attach(new SquadronComponent("Cyclops"));
+            entity.Attach(new NonPlayerPilotComponent());
+
+            entity.Get<TransformComponent>().Matrix.RotateX(uniform(-PI, PI));
+            entity.Get<TransformComponent>().Matrix.RotateY(uniform(-PI, PI));
+            entity.Get<TransformComponent>().Matrix.RotateZ(uniform(-PI, PI));
+            entity.Get<TransformComponent>().Position = new Vector3(x[i], 2000, 1000);
+            entity.Get<SoundComponent>().LoopPlay();
+        }
+    }
+}
