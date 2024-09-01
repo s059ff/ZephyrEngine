55da75f96e5f270930ebdd0e3651a5d1c1ad766b
s059ff <s059ff@gmail.com>
2024-09-01 21:23:06 +0900

    zephyr::tuple 名前空間を実質 std::tuple に変更

--- zephyr/tuple.h
+++ zephyr/tuple.h
@@ -1,34 +1,8 @@
 ﻿#pragma once
 
+#include <tuple>
+
 namespace zephyr
 {
-    template <typename T>
-    struct tuple1
-    {
-        T item1;
-    };
-
-    template <typename T1, typename T2>
-    struct tuple2
-    {
-        T1 item1;
-        T2 item2;
-    };
-
-    template <typename T1, typename T2, typename T3>
-    struct tuple3
-    {
-        T1 item1;
-        T2 item2;
-        T3 item3;
-    };
-
-    template <typename T1, typename T2, typename T3, typename T4>
-    struct tuple4
-    {
-        T1 item1;
-        T2 item2;
-        T3 item3;
-        T4 item4;
-    };
+    using namespace std;
 }
